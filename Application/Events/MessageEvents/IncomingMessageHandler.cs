104a80991629698509dbc3d425fb1e8d79f48885
Markus K <github@markusk.dev>
2022-10-04 21:44:49 +0200

    :construction: Add basic incoming handler

---
+++ Application/Events/MessageEvents/IncomingMessageHandler.cs
@@ -0,0 +1,25 @@
+﻿using MediatR;
+using MimeKit;
+
+namespace SmtpForwarder.Application.Events.MessageEvents;
+
+public record IncomingMessageRequest(MimeMessage Message) : IRequest<IncomingMessageResponse>;
+
+public class IncomingMessageHandler : IRequestHandler<IncomingMessageRequest, IncomingMessageResponse>
+{
+
+    public async Task<IncomingMessageResponse> Handle(IncomingMessageRequest request, CancellationToken cancellationToken)
+    {
+        return IncomingMessageResponse.Ok;
+    }
+}
+
+public enum IncomingMessageResponse
+{
+    Ok,
+    MailboxNameNotAllowed,
+    MailboxUnavailable,
+    NoValidRecipientsGiven,
+    SizeLimitExceeded,
+    Error,
+}
